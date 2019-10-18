caccf991735adbb4c5b7eb209928367b17c41d75
Frank <weibo@wanglipeng.me>
2019-10-18 17:44:37 +0800

    change log -> logger

--- logger.go
+++ logger.go
@@ -20,42 +20,41 @@ type Logger struct {
 }
 
 func (logger *Logger) Debug(msg string) (n int, err error) {
-	log.err_level = DEBUG
-	return log.Write([]byte(msg))
+	logger.err_level = DEBUG
+	return logger.Write([]byte(msg))
 }
 
 func (logger *Logger) Notice(msg string) (n int, err error) {
-	log.err_level = NOTICE
-	n, err = log.Write([]byte(msg))
+	logger.err_level = NOTICE
+	n, err = logger.Write([]byte(msg))
 	return n, err
 }
 
 func (logger *Logger) Warning(msg string) (n int, err error) {
-	log.err_level = WARNING
-	n, err = log.Write([]byte(msg))
+	logger.err_level = WARNING
+	n, err = logger.Write([]byte(msg))
 	return n, err
 }
 
 func (logger *Logger) Error(msg string) (n int, err error) {
-	log.err_level = ERROR
-	n, err = log.Write([]byte(msg))
+	logger.err_level = ERROR
+	n, err = logger.Write([]byte(msg))
 	return n, err
 }
 
 func (logger *Logger) Write(msg []byte) (n int, err error) {
-	if log.Device == nil {
+	if logger.Device == nil {
 		panic("Write log msg to device error. check device setup.")
 	}
-	if log.Level == 0 {
-		log.Level = ERROR | NOTICE | DEBUG | WARNING
+	if logger.Level == 0 {
+		logger.Level = ERROR | NOTICE | DEBUG | WARNING
 	}
-	//fmt.Println(logger.err_level, log.Level&log.err_level)
-	if log.Level&log.err_level <= 0 {
+	if logger.Level&logger.err_level <= 0 {
 		return
 	}
-	var levelStr string = log.levelToString(logger.err_level)
+	var levelStr string = logger.levelToString(logger.err_level)
 	msg = []byte(fmt.Sprintf("[%s] %s %s\n", levelStr, time.Now().Format("2006-01-01 15:04:05"), msg))
-	n, err = log.Device.Write(msg)
+	n, err = logger.Device.Write(msg)
 	return n, err
 }
 
